3b3662b7a3fc65f38e93c999e7ebf838281c8c4e
Christian Heckendorf <heckendorfc@gmail.com>
2013-09-22 12:11:46 -0400

    Fixing database create script to use the correct database.

--- database/create.sql
+++ database/create.sql
@@ -2,6 +2,8 @@ create database plm;
 create user 'plm_user'@'localhost' identified by 'plm_password';
 grant all on plm.* to 'plm_user'@'localhost';
 
+use plm;
+
 drop table User;
 create table User(
 id int auto_increment primary key,
