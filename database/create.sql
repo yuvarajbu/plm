5f78c54e8c46e6155dca2adb7fae04bd618989e6
Christian Heckendorf <heckendorfc@gmail.com>
2013-09-29 16:15:29 -0400

    Adding basic database structure.

--- database/create.sql
+++ database/create.sql
@@ -4,9 +4,69 @@ grant all on plm.* to 'plm_user'@'localhost';
 
 use plm;
 
-drop table User;
-create table User(
-id int auto_increment primary key,
-name varchar(50) not null,
-password varchar(50) not null
+create table `User`(
+	id int auto_increment primary key,
+	name varchar(50) not null,
+	password varchar(50) not null
+);
+
+create table `Role`(
+	id int auto_increment primary key,
+	name varchar(50) not null
+);
+
+create table `UserRole`(
+	id int auto_increment primary key,
+	userId int,
+	roleId int,
+	foreign key (userId) references `User`(id),
+	foreign key (roleId) references `Role`(id)
+);
+
+create table `Project`(
+	id int auto_increment primary key,
+	name varchar(50) not null
+);
+
+create table `ProjectUser`(
+	id int auto_increment primary key,
+	userId int,
+	projectId int,
+	foreign key (userId) references `User`(id),
+	foreign key (projectId) references `Project`(id)
+);
+
+create table `Release`(
+	id int auto_increment primary key,
+	version varchar(50) not null,
+	projectId int,
+	foreign key (projectId) references `Project`(id)
+);
+
+create table `UserStory`(
+	id int auto_increment primary key,
+	name varchar(50) not null,
+	description varchar(200),
+	releaseId int,
+	foreign key (releaseId) references `Release`(id)
+);
+
+create table `Bug`(
+	id int auto_increment primary key,
+	name varchar(50) not null,
+	description varchar(200),
+	createdId int,
+	assignedId int,
+	foreign key (createdId) references `User`(id),
+	foreign key (assignedId) references `User`(id)
+);
+
+create table `Task`(
+	id int auto_increment primary key,
+	name varchar(50) not null,
+	description varchar(200),
+	userStoryId int,
+	assignedId int,
+	foreign key (userStoryId) references `UserStory`(id),
+	foreign key (assignedId) references `User`(id)
 );
---
+++ database/drop.sql
@@ -0,0 +1,11 @@
+use plm;
+
+drop table if exists `Task`;
+drop table if exists `Bug`;
+drop table if exists `UserStory`;
+drop table if exists `Release`;
+drop table if exists `ProjectUser`;
+drop table if exists `Project`;
+drop table if exists `UserRole`;
+drop table if exists `Role`;
+drop table if exists `User`;
